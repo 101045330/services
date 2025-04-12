479a5d6192363376c009c0e911da14dc81f8b0cb
101045330 <101045330@georgebrown.ca>
2025-04-12 22:37:06 +0000

    created components: navigation, home, all-products-category-products, product-details

--- bestbuy/src/app/app.module.ts
+++ bestbuy/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { CategoryProductsComponent } from './category-products/category-products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    AllProductsComponent,
+    CategoryProductsComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
