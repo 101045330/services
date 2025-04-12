0b71ed5f77af0c597b95ae5370c3a1ed069c196c
101045330 <101045330@georgebrown.ca>
2025-04-12 23:47:14 +0000

    create a new project and added dependencies

--- bestbuy-app/src/app/app.component.spec.ts
+++ bestbuy-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bestbuy'`, () => {
+  it(`should have as title 'bestbuy-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bestbuy');
+    expect(app.title).toEqual('bestbuy-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bestbuy');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bestbuy-app');
   });
 });
