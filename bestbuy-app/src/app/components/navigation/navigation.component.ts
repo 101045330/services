a213e861d3fa6011b817345cec1e40f3424fbe35
101045330 <101045330@georgebrown.ca>
2025-04-13 02:01:15 +0000

    image fix pending

--- bestbuy-app/src/app/components/navigation/navigation.component.ts
+++ bestbuy-app/src/app/components/navigation/navigation.component.ts
@@ -11,21 +11,11 @@ export class NavigationComponent implements OnInit {
   constructor(private router: Router) { }
   ngOnInit(): void { }
 
-  /*onCategoryChange(category: string) {
-    if (category) {
-      this.router.navigate(['/product-categories', category]);
-    }
-  }*/
-/*
-  onCategoryChange(event: Event): void {
-    const selectElement = event.target as HTMLSelectElement;
-    const category = selectElement.value;
-    if (category) {
-      this.router.navigate(['/product-categories', category]);
-    }
-  }
-*/
-
+  //  Method to handle category change
+  // This method is triggered when the user selects a category from the dropdown
+  // It retrieves the selected category and navigates to the corresponding route
+  // The route is constructed using the selected category value
+  // The method uses optional chaining to handle the case where the select element is null
   onCategoryChange(event: Event): void {
     const selectElement = event.target as HTMLSelectElement | null;
     const category = selectElement?.value; // Use optional chaining to handle null
